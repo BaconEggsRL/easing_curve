1f9e2e0d009b22a102b92310aa72390375dd7969
BaconEggsRL <apatton090@gmail.com>
2026-08-20 18:53:30 -0400

    Update cleanup.gd

--- addons/editor_settings_cleanup/cleanup.gd
+++ addons/editor_settings_cleanup/cleanup.gd
@@ -2,14 +2,29 @@
 extends EditorPlugin
 
 
-func _enter_tree() -> void:
+const PLUGIN_NAME := "GDQuest GDScript Formatter"
+const PLUGIN_PREFIX := "gdquest_gdscript_formatter/"
+
+
+func _enable_plugin() -> void:
+	var removed := erase_settings_with_prefix(PLUGIN_PREFIX)
+
+	print(
+		"Finished removing %s settings. Removed: %d"
+		% [PLUGIN_NAME, removed]
+	)
+
+
+static func erase_settings_with_prefix(prefix: String) -> int:
 	var settings := EditorInterface.get_editor_settings()
+	var removed := 0
 
 	for property in settings.get_property_list():
 		var setting_name := str(property.get("name", ""))
 
-		if setting_name.begins_with("gdquest_gdscript_formatter/"):
-			print("Removing stale EditorSetting: ", setting_name)
+		if setting_name.begins_with(prefix):
+			print("Removing EditorSetting: ", setting_name)
 			settings.erase(setting_name)
+			removed += 1
 
-	print("Finished removing GDQuest GDScript Formatter settings.")
+	return removed
