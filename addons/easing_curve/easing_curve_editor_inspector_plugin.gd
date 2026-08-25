1144397d914a00fd06b6a7c23c1fcfcf7b96c7ff
BaconEggsRL <apatton090@gmail.com>
2026-08-25 14:58:26 -0400

    Update easing_curve_editor_inspector_plugin.gd

--- addons/easing_curve/easing_curve_editor_inspector_plugin.gd
+++ addons/easing_curve/easing_curve_editor_inspector_plugin.gd
@@ -947,6 +947,7 @@ class PointsFoldableSection:
 		)
 		if ClassDB.class_exists(&"FoldableContainer"):
 			_native_section = ClassDB.instantiate(&"FoldableContainer") as Control
+			_native_section.focus_mode = Control.FOCUS_NONE
 			_native_section.set(&"title", section_title)
 			_native_section.set(&"title_text_overrun_behavior", TextServer.OVERRUN_TRIM_ELLIPSIS)
 			_native_section.set(&"folded", initially_folded)
