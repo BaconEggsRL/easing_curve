f9ad2f3aeb2a999def7a1b16922e602a36427c72
daflamingpotato <apatton090@gmail.com>
2026-09-08 16:43:12 -0400

    link

--- addons/easing_curve/scripts/editor/inspector/inspector_curve_context.gd
+++ addons/easing_curve/scripts/editor/inspector/inspector_curve_context.gd
@@ -984,11 +984,11 @@ func _create_point_add_controls() -> Control:
 
 
 func _add_new_point_handle_mode_items(option: OptionButton) -> void:
-	option.add_item("Free", EasingCurvePoint.HandleMode.FREE)
-	option.add_item("Linear", EasingCurvePoint.HandleMode.LINEAR)
-	option.add_item("Balanced", EasingCurvePoint.HandleMode.BALANCED)
-	option.add_item("Mirrored", EasingCurvePoint.HandleMode.MIRRORED)
-	option.add_item("Linked", EasingCurvePoint.HandleMode.LINKED)
+	option.add_icon_item(EDITOR_THEME_CACHE.get_icon(&"BezierHandlesFree"), "Free", EasingCurvePoint.HandleMode.FREE)
+	option.add_icon_item(EDITOR_THEME_CACHE.get_icon(&"BezierHandlesLinear"), "Linear", EasingCurvePoint.HandleMode.LINEAR)
+	option.add_icon_item(EDITOR_THEME_CACHE.get_icon(&"BezierHandlesBalanced"), "Balanced", EasingCurvePoint.HandleMode.BALANCED)
+	option.add_icon_item(EDITOR_THEME_CACHE.get_icon(&"BezierHandlesMirror"), "Mirrored", EasingCurvePoint.HandleMode.MIRRORED)
+	option.add_icon_item(preload("res://addons/easing_curve/assets/BezierHandlesLinked.svg"), "Linked", EasingCurvePoint.HandleMode.LINKED)
 
 
 func _measure_new_point_handle_mode_size() -> Vector2:
